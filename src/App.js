96fcebefccfa2c7e156f800efc96d1a4bed5808b
jmalochee <jmalochee@gmail.com>
2020-04-27 23:46:14 -0400

    grid layout

--- src/App.js
+++ src/App.js
@@ -11,7 +11,39 @@ function App() {
       </header>
       <body className="App-body">
         <div className="Calculator">
-          App here
+          <div className="Display">
+             ticker 
+          </div>
+            <table className="Keypad">
+              <tr>
+                <td className="Unit"> big </td>
+                <td> 1 </td>
+                <td> 2 </td>
+                <td> 3 </td>
+                <td className="Operator"> / </td>
+              </tr>
+              <tr>
+                <td className="Unit"> med </td>
+                <td> 4 </td>
+                <td> 5 </td>
+                <td> 6 </td>
+                <td className="Operator"> x </td>
+              </tr>
+              <tr>
+                <td className="Unit"> small </td>
+                <td> 7 </td>
+                <td> 8 </td>
+                <td> 9 </td>
+                <td className="Operator">- </td>
+              </tr>
+              <tr>
+                <td> ac </td>
+                <td> . </td>
+                <td> 0 </td>
+                <td> x/y </td>
+                <td className="Operator"> + </td>
+              </tr>
+            </table>
         </div>
       </body>
     </div>
