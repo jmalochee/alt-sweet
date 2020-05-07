329ac63a58eb9092fcab838209039d53ff8b9efc
jmalochee <jmalochee@gmail.com>
2020-05-07 13:39:53 -0400

    ac handler and input handler also Key/div to Button/button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import Key from "./Key";
+import Button from "./Button";
 import Display from "./Display";
 
 class Calculator extends Component {
@@ -10,51 +10,56 @@ class Calculator extends Component {
 		}
 	}
 	
-	buttonHandler = (event, value) => {
+	buttonHandler = (event) => {
 		let string = this.state.display
-		this.setState({ display: string.concat(value) })
+		this.setState({ display: string.concat(event.target.value) })
 	}
 
 	keyPressHandler = (event) => {
 		let string = this.state.display
 		this.setState({ display: string.concat(event.key) })
+		console.log(event)
+	}
+
+	inputHandler = (event) => {
+		console.log(event.target.value)
+		if(event.target.value === "." || !isNaN(event.target.value))
+			this.buttonHandler(event)
+		else if(event.target.value === "ac")
+			this.acHandler()
 	}
 
 	componentDidMount(){
     document.addEventListener("keypress", this.keyPressHandler);
 	}
 
+	acHandler = () => {
+		this.setState({display: ""})
+	}
+
 	render() {
 		//the style choice here mimics keypad arrangement
-		const keyProps = [
-			[["ac","AC"],["1","Number"],["2","Number"],["3","Number"],[" / ","Operator"]],
-			[["small","Unit"],["4","Number"],["5","Number"],["6","Number"],[" x ","Operator"]],
-			[["med","Unit"],["7","Number"],["8","Number"],["9","Number"],[" - ","Operator"]],
-			[["big","Unit"],[".","Number"],["0","Number"],["x/y","Number"],[" + ","Operator"]]
+		const btnProps = [
+			["ac","AC"],["1","Number"],["2","Number"],["3","Number"],[" / ","Operator"],
+			["small","Unit"],["4","Number"],["5","Number"],["6","Number"],[" x ","Operator"],
+			["med","Unit"],["7","Number"],["8","Number"],["9","Number"],[" - ","Operator"],
+			["big","Unit"],[".","Number"],["0","Number"],["x/y","Number"],[" + ","Operator"]
 		];
 
-		const keyLayout = keyProps.map((row, index) => (
-			<tr key={index} >
-				{row.map((key, index) => (
-					<Key
-						key={index}
-						keyType={key[1]}
-						keyValue={key[0]}
-						handlerFunction={(event) => (this.buttonHandler(event, key[0]))}
-					/>
-				))}
-			</tr>
+		const btnLayout = btnProps.map((btn, index) => (
+			<Button
+				key={index}
+				btnType={btn[1]}
+				btnValue={btn[0]}
+				handlerFunction={(event) => this.inputHandler(event)}
+			/>
 		))
 
 		return(
 			<div id="Calculator">
 				<Display value={this.state.display}/>
-				<div>
-					<table id="Keypad">
-						<tbody>
-							{keyLayout}
-		    		</tbody>
-		      </table>
+				<div id="Keypad">
+					{btnLayout}
 		    </div>
 	    </div>
 		)
